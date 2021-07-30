fb99798a32e22d376c5e180dd32e37ff86f44c55
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-07-30 11:33:07 -0400

    firstCommit

---
+++ dayfournotes/src/main/java/com/hcl/ojttraining/dayone/julythti/dayfournotes/App.java
@@ -0,0 +1,13 @@
+package com.hcl.ojttraining.dayone.julythti.dayfournotes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dayfournotes/src/main/java/com/hcl/ojttraining/dayone/julythti/dayfournotes/exercise1/Exercise1.java
@@ -0,0 +1,5 @@
+package com.hcl.ojttraining.dayone.julythti.dayfournotes.exercise1;
+
+public class Exercise1 {
+
+}
